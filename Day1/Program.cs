166b9a1219b7d69f331aa62c6f22ba05006f15d2
Makaa88 <38141680+Makaa88@users.noreply.github.com>
2018-12-02 15:28:09 +0100

    Add files via upload

---
+++ Day1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digit = 0;
+            int sum = 0;
+            var lines = System.IO.File.ReadAllLines("dane.dat");
+            foreach(var line in lines)
+            {
+                sum += int.Parse(line);
+            }
+            Console.WriteLine("SUMA: {0}", sum);
+
+            sum = 0;
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+            freq.Add(sum, 1);
+            bool flag = true;
+            while(flag)
+            {
+                foreach(var line in lines)
+                {
+                    sum += int.Parse(line);
+                    if(freq.ContainsKey(sum))
+                    {
+                        digit = sum;
+                        flag = false;
+                        break;
+                    }
+                    else
+                    {
+                        freq.Add(sum, 1);
+                    }
+                }
+            }
+
+            Console.WriteLine("FREQ: {0}", digit);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = { 0, 0 };
+            string[] stringTab = new string[2];
+            var lines = File.ReadAllLines("dane.dat");//string[]
+            foreach(var line in lines)//string
+            {
+                bool two = true;
+                bool three = true;
+                foreach (char sign in line)
+                {
+                   int number = line.Count(f => f == sign);
+                   if(number == 2 && two)
+                    {
+                        tab[0]++;
+                        two = false;
+                    }
+                   else if(number == 3 && three)
+                    {
+                        tab[1]++;
+                        three = false;
+                    }
+                }
+            }
+            Console.WriteLine("RESULT: {0}", tab[0]*tab[1]);
+            bool flag = false;
+            for(int i = 0; i < lines.Length-1; i++)
+            {
+                stringTab[0] = lines[i];
+                string first = lines[i];
+                for(int j = i+1; j < lines.Length; j++)
+                {
+                    int div = 0;
+                    string temp = lines[j];
+                    for(int k = 0; k < stringTab[0].Length; k++)
+                    {
+                        if (first[k] != temp[k]) div++;
+                    }
+
+                    if (div == 1)
+                    {
+                        stringTab[1] = lines[j];
+                        flag = true;
+                    }
+                    
+                }
+                if (flag) break;
+            }
+
+            Console.WriteLine("RESULT: {0}", stringTab[0]);
+            Console.WriteLine("RESULT: {0}", stringTab[1]);
+            string t1 = stringTab[0];
+            string t2 = stringTab[1];
+            for(int i = 0; i < t1.Length; i++)
+            {
+                if (t1[i] == t2[i]) Console.Write("{0}", t1[i]);
+            }
+
+
+            Console.ReadKey();
+        } 
+    }
+}
